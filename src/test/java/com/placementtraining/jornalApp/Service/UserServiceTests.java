93603b2793a08dbf44adbd7ecfeb6ea6c13c9c3d
amit pratap singh <amitrajvi773@gmail.com>
2026-06-05 13:49:54 +0530

    update

--- src/test/java/com/placementtraining/jornalApp/Service/UserServiceTests.java
+++ src/test/java/com/placementtraining/jornalApp/Service/UserServiceTests.java
@@ -1,14 +1,35 @@
 package com.placementtraining.jornalApp.Service;
-
+import com.placementtraining.jornalApp.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-
+@SpringBootTest
 public class UserServiceTests {
+    @Autowired
+    UserRepository userRepository;
     @Test
     public void testAdd(){
         assertEquals(4,2+1);
     }
+//    @Test if we use parameterixed then this is not used
+    @ParameterizedTest
+    @CsvSource({
+            "amit",
+            "vivek",
+            "dev"
+    })
+    public void testFindbyUsername(String name){
+
+        assertNotNull(userRepository.findByUsername(name));
+    }
+
+
 
 }
