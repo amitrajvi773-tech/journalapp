39b7f5c935701633562ef1411dc33a84eded64b5
amit pratap singh <amitrajvi773@gmail.com>
2026-06-05 12:47:17 +0530

    update

---
+++ src/test/java/com/placementtraining/jornalApp/Service/UserServiceTests.java
@@ -0,0 +1,14 @@
+package com.placementtraining.jornalApp.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class UserServiceTests {
+    @Test
+    public void testAdd(){
+        assertEquals(4,2+1);
+    }
+
+}
